55f74baa8c7ae9055eb9738e3f0f8a59d0a0687f
ksmanoj3221 <ksmanoj3221@gmail.com>
2024-10-12 16:01:23 +0530

    Added Joke Model

---
+++ Jokes/Models/Joke.cs
@@ -0,0 +1,9 @@
+﻿namespace Jokes.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+        public string JokeQuestion { get; set; }
+        public string JokeAnswer { get; set; }
+    }
+}
