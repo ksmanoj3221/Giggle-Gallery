3bba36dc7da67d9a47526cbc16c7df1c6093a39b
ksmanoj3221 <ksmanoj3221@gmail.com>
2024-10-12 16:02:41 +0530

    Added ctor in Joke Model

--- Jokes/Models/Joke.cs
+++ Jokes/Models/Joke.cs
@@ -5,5 +5,10 @@
         public int Id { get; set; }
         public string JokeQuestion { get; set; }
         public string JokeAnswer { get; set; }
+
+        public Joke()
+        {
+            
+        }
     }
 }
